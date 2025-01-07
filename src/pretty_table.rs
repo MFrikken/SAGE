96c1f8e5e2d66eae92a6bcba8e86eb16411e04e3
MFrikken <milanfrinken@web.de>
2025-01-07 16:06:44 +0100

    Added color coding to severity level of vulnerabilities: Low = green, Medium = yellow, High = Red

--- src/pretty_table.rs
+++ src/pretty_table.rs
@@ -1,4 +1,4 @@
-use comfy_table::{ColumnConstraint::{UpperBoundary, LowerBoundary}, Width::*, modifiers::{UTF8_ROUND_CORNERS, UTF8_SOLID_INNER_BORDERS}, presets::UTF8_FULL, ContentArrangement, Table};
+use comfy_table::{ColumnConstraint::{UpperBoundary, LowerBoundary}, Width::*, modifiers::{UTF8_ROUND_CORNERS, UTF8_SOLID_INNER_BORDERS}, presets::UTF8_FULL, ContentArrangement, Table, Cell, Color};
 
 
 pub fn write_vulnerabilities_table (vulnerabilities: &mut Vec<Vec<String>>) {
@@ -35,7 +35,50 @@ pub fn write_vulnerabilities_table (vulnerabilities: &mut Vec<Vec<String>>) {
     .set_constraint(UpperBoundary(Fixed(20)));
 
     for vulnerability in vulnerabilities {
-        table.add_row(vulnerability);
+
+        let severity = &vulnerability[5];
+        let mut severity_cell = Cell::new(severity);
+        severity_cell = match severity.as_str() {
+            "Low" => severity_cell.fg(Color::Green),
+            "Medium" => severity_cell.fg(Color::Yellow),
+            "High" => severity_cell.fg(Color::Red),
+            _ => severity_cell,
+        };
+
+        table.add_row(vec![
+            Cell::new(&vulnerability[0]),
+            Cell::new(&vulnerability[1]),
+            Cell::new(&vulnerability[2]),
+            Cell::new(&vulnerability[3]),
+            Cell::new(&vulnerability[4]),
+            severity_cell,
+            Cell::new(&vulnerability[6]),
+            Cell::new(&vulnerability[7]),
+            Cell::new(&vulnerability[8]),
+            ]);
+    }
+
+    println!("{table}");
+}
+
+pub fn write_weaknesses_table (weaknesses: &mut Vec<Vec<String>>) {
+    let mut table: Table = Table::new();
+
+    table
+    .load_preset(UTF8_FULL)
+    .apply_modifier(UTF8_ROUND_CORNERS)
+    .apply_modifier(UTF8_SOLID_INNER_BORDERS)
+    .set_content_arrangement(ContentArrangement::Dynamic)
+    .set_header(vec![
+        "ID",
+        "Type",
+        "Name",
+        "Value",
+        "References"
+    ]);
+
+    for weakness in weaknesses {
+        table.add_row(weakness);
     }
 
     println!("{table}");
